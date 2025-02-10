73d341bb9dd8e460bb4bc7298f15afd9456237b8
RACELESS-RLS <ncbandtlow@gmail.com>
2025-02-10 01:18:12 -0500

    balance correction on delivery. More distance based

--- lua/ge/extensions/career/modules/delivery/generator.lua
+++ lua/ge/extensions/career/modules/delivery/generator.lua
@@ -125,8 +125,8 @@ end
 local parcelItemMoneyMultiplier = 1
 local function getMoneyRewardForParcelItem(item, distance)
   local basePrice = math.sqrt(item.slots) / 4
-  local distanceExp = 0.4 + math.sqrt(item.slots)/100
-  local pricePerM = 2 + math.pow(item.weight, 0.7)
+  local distanceExp = 1.25 + math.sqrt(item.slots)/100
+  local pricePerM = 2 + math.pow(item.weight, 0.53)
   --local modMultiplier = 1---0.9 + 0.1 * #item.modifiers
   --for _, mod in ipairs(item.modifiers) do
   --  modMultiplier = modMultiplier * (mod.moneyMultipler or 1)
