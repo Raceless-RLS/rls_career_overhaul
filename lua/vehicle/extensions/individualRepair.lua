678c02d51930a140d7192c9445b2b0a6a60209ac
TristanGaldys <123184923+TristanGaldys@users.noreply.github.com>
2025-06-02 23:31:37 -0400

    - Fixed Removing Damaged Parts

--- lua/vehicle/extensions/individualRepair.lua
+++ lua/vehicle/extensions/individualRepair.lua
@@ -22,10 +22,12 @@ end
 -- @param pathsTable (table) Table of paths to match against
 -- @return (boolean) True if the part should be repaired
 local function shouldProcessPart(partPath, pathsTable)
-  if not partPath or not pathsTable then return false end
-  
+  if not partPath or not pathsTable then
+    return false
+  end
+
   for checkPath, _ in pairs(pathsTable) do
-    if partPath:find(checkPath, 1, true) then
+    if partPath == checkPath then
       return true
     end
   end
@@ -63,15 +65,17 @@ function M.loadVehicleStateSelectiveRepair(filename, partsToRepair, partsToRemov
     log("E", "individualRepair.load", "Unable to load vehicle: Wrong vehicle. File: " .. filename)
     return
   end
-  
+
   -- Compare structure sizes but don't abort - just log a warning
   if save.nodeCount ~= tableSizeC(v.data.nodes) or save.beamCount ~= tableSizeC(v.data.beams) then
-    log("W", "individualRepair.load", "Structure mismatch detected - likely due to part replacements. Proceeding with caution.")
+    log("W", "individualRepair.load",
+      "Structure mismatch detected - likely due to part replacements. Proceeding with caution.")
   end
 
   -- Ensure beamstate.onBeamDeformed is available
   if not beamstate or not beamstate.onBeamDeformed then
-    log("E", "individualRepair.load", "beamstate.onBeamDeformed is not available. Cannot reliably load beam deformation states.")
+    log("E", "individualRepair.load",
+      "beamstate.onBeamDeformed is not available. Cannot reliably load beam deformation states.")
     return
   end
 
@@ -80,7 +84,7 @@ function M.loadVehicleStateSelectiveRepair(filename, partsToRepair, partsToRemov
   for _, path in ipairs(partsToRepair) do
     partsToRepairSet[path] = true
   end
-  
+
   -- Create a set of part paths to remove
   local partsToRemoveSet = {}
   if partsToRemove then
@@ -96,27 +100,31 @@ function M.loadVehicleStateSelectiveRepair(filename, partsToRepair, partsToRemov
   for cid = 0, tableSizeC(v.data.nodes) - 1 do
     local node = v.data.nodes[cid]
     local nodePath = node.partPath or v.config.partsTree.partPath
-    
+
     if shouldProcessPart(nodePath, partsToRepairSet) then
       if not partNodes[nodePath] then
         partNodes[nodePath] = {}
       end
-      table.insert(partNodes[nodePath], {cid = cid, pos = node.pos})
+      table.insert(partNodes[nodePath], {
+        cid = cid,
+        pos = node.pos
+      })
     end
   end
-  
+
   -- Get vehicle's current transform
   local refNodeID = v.data.refNodes[0].ref
   local currentRefPos = obj:getNodePosition(refNodeID)
   local vehRot = obj:getRotation() -- Use direct rotation instead of constructing from vectors
-  
+
   -- Get original reference node position from jbeam definition
   local origRefNodePos = v.data.nodes[refNodeID].pos
-  
+
   if hydros and hydros.hydros and save.hydros then
     for k, h_state in pairs(save.hydros) do
       local hydroPath = hydros.hydros[k].partPath
-      if hydros.hydros[k] and not shouldProcessPart(hydroPath, partsToRepairSet) and not shouldProcessPart(hydroPath, partsToRemoveSet) then
+      if hydros.hydros[k] and not shouldProcessPart(hydroPath, partsToRepairSet) and
+        not shouldProcessPart(hydroPath, partsToRemoveSet) then
         hydros.hydros[k].state = h_state
       end
     end
@@ -137,13 +145,13 @@ function M.loadVehicleStateSelectiveRepair(filename, partsToRepair, partsToRemov
     end
     -- If node doesn't exist, we silently skip it (this is now OK with mismatched structures)
   end
-  
+
   -- Now position the parts to repair, treating each part as a unit
   for partPath, nodes in pairs(partNodes) do
     for _, nodeData in ipairs(nodes) do
-      local cid = nodeData.cid      
+      local cid = nodeData.cid
       obj:setNodePosition(cid, obj:getNodePosition(cid))
-      
+
       -- Set mass from saved data if available, only if this specific node exists in the save
       if save.nodes[tostring(cid + 1)] and #save.nodes[tostring(cid + 1)] > 1 then
         obj:setNodeMass(cid, save.nodes[tostring(cid + 1)][2])
@@ -154,7 +162,7 @@ function M.loadVehicleStateSelectiveRepair(filename, partsToRepair, partsToRemov
   -- Load beam states, with selective repair - safely handling missing beams
   for cid_plus_1, beam_data in pairs(save.beams) do
     local cid = tonumber(cid_plus_1) - 1
-    if v.data.beams[cid] then  -- Only process beams that exist in current vehicle
+    if v.data.beams[cid] then -- Only process beams that exist in current vehicle
       local beamPath = v.data.beams[cid].partPath or v.config.partsTree.partPath
       if shouldProcessPart(beamPath, partsToRepairSet) then
         -- For beams we want to repair, reset them to original state
@@ -163,7 +171,8 @@ function M.loadVehicleStateSelectiveRepair(filename, partsToRepair, partsToRemov
           obj:setBeamLength(cid, beamData.beamLength)
         end
         if beamData.beamSpring then
-          obj:setBeamSpringDamp(cid, beamData.beamSpring, beamData.beamDamp, beamData.beamShortBound or -1, beamData.beamLongBound or -1)
+          obj:setBeamSpringDamp(cid, beamData.beamSpring, beamData.beamDamp, beamData.beamShortBound or -1,
+            beamData.beamLongBound or -1)
         end
         if beamData.beamDeformed then
           beamstate.onBeamDeformed(cid, 0)
@@ -187,7 +196,7 @@ function M.loadVehicleStateSelectiveRepair(filename, partsToRepair, partsToRemov
     -- If this beam doesn't have data in the save file, it's from a new/replaced part
     if not save.beams[tostring(cid + 1)] then
       local beamPath = v.data.beams[cid].partPath or v.config.partsTree.partPath
-      
+
       -- For new beams in parts we're not explicitly repairing, set to default state
       if not shouldProcessPart(beamPath, partsToRepairSet) and not shouldProcessPart(beamPath, partsToRemoveSet) then
         local beamData = v.data.beams[cid]
@@ -195,7 +204,8 @@ function M.loadVehicleStateSelectiveRepair(filename, partsToRepair, partsToRemov
           obj:setBeamLength(cid, beamData.beamLength)
         end
         if beamData.beamSpring then
-          obj:setBeamSpringDamp(cid, beamData.beamSpring, beamData.beamDamp, beamData.beamShortBound or -1, beamData.beamLongBound or -1)
+          obj:setBeamSpringDamp(cid, beamData.beamSpring, beamData.beamDamp, beamData.beamShortBound or -1,
+            beamData.beamLongBound or -1)
         end
         if beamData.beamDeformed then
           beamstate.onBeamDeformed(cid, 0)
@@ -247,11 +257,30 @@ local function seperateByPart(inputTable)
   return outputTable
 end
 
+local function getWheelNodes()
+  local wheelNodes = {}
+  for i, wheel in pairs(v.data.wheels) do
+    if wheel.nodes then
+      for index, node in ipairs(wheel.nodes) do
+        table.insert(wheelNodes, node, "wheel" .. i .. "_node" .. index)
+      end
+    end
+    if wheel.treadNodes then
+      for index, node in ipairs(wheel.treadNodes) do
+        table.insert(wheelNodes, node, "wheel" .. i .. "_treadNode" .. index)
+      end
+    end
+  end
+  return wheelNodes
+end
+
 local function saveDamageData(filename)
   if filename == nil then
     filename = v.data.vehicleDirectory .. "/vehicle.rls_save.json"
   end
-  
+
+  local wheelNodes = getWheelNodes()
+
   local save = {}
   save.format = "rls_v1"
   save.model = v.data.model
@@ -259,8 +288,8 @@ local function saveDamageData(filename)
   save.rotation = quat(obj:getRotation()):toTable()
 
   save.partStates = {}
-  --
 
+  --
   for _, node in pairs(v.data.nodes) do
     local parentPath
     if not node.partPath then
@@ -274,10 +303,18 @@ local function saveDamageData(filename)
     if not save.partStates[parentPath].nodes then
       save.partStates[parentPath].nodes = {}
     end
+    if not node.name then
+      if wheelNodes[node.cid] then
+        node.name = wheelNodes[node.cid]
+      else
+        print("No name found for: " .. tostring(node.cid) .. " " .. tostring(node.partPath))
+      end
+    end
     table.insert(save.partStates[parentPath].nodes, node.cid, {
       pos = obj:getNodePosition(node.cid):toTable(),
       name = node.name,
-      mass = math.abs(obj:getOriginalNodeMass(node.cid) - obj:getNodeMass(node.cid)) > 0.1 and obj:getNodeMass(node.cid) or nil
+      mass = math.abs(obj:getOriginalNodeMass(node.cid) - obj:getNodeMass(node.cid)) > 0.1 and obj:getNodeMass(node.cid) or
+        nil
     })
   end
 
@@ -298,7 +335,7 @@ local function saveDamageData(filename)
       length = obj:getBeamRestLength(beam.cid),
       broken = obj:beamIsBroken(beam.cid) and true or nil,
       deformed = obj:getBeamDeformation(beam.cid) ~= 0 and obj:getBeamDeformation(beam.cid) or nil,
-      idents = {beam.id1, beam.id2, beam.beamType},
+      idents = {beam.id1, beam.id2, beam.beamType}
     })
   end
 
@@ -317,10 +354,10 @@ local function saveDamageData(filename)
     end
     table.insert(save.partStates[parentPath].hydros, hydros.cid, {
       idents = {hydros.id1, hydros.id2, hydros.beamType},
-      state = hydros.state,
+      state = hydros.state
     })
   end
-  --]]
+  -- ]]
 
   --[[
 
@@ -368,7 +405,7 @@ local function saveDamageData(filename)
     ::nextPart::
   end
   --]]
-  
+
   local savedParts = 0
   for partPath, state in pairs(save.partStates) do
     savedParts = savedParts + 1
@@ -382,35 +419,37 @@ end
 local function loadNodeIDconvert(separateOldNodes)
   local separateNewNodes = seperateByPart(v.data.nodes)
   local nodeConvert = {}
-  
+
+  local wheelNodes = getWheelNodes()
+
   local partCount = 0
   -- Count the actual number of entries in the table
   local totalParts = 0
   for _ in pairs(separateOldNodes) do
     totalParts = totalParts + 1
   end
-  
-  dumpz(separateOldNodes, 1)
+
   print("Total parts to process: " .. totalParts)
 
   for partPath, state in pairs(separateOldNodes) do
-    local nodeCount = 0
-    local nodeStatement = ""
     partCount = partCount + 1
-    print("Part: " .. partPath .. " (" .. partCount .. "/" .. totalParts .. ")")
     local nodes = state.nodes
     if not nodes then
       print("No nodes found for part: " .. partPath)
       goto continue -- Using goto instead of break to continue the loop
     end
     for cid, oldNode in pairs(nodes) do
-      local oldName = oldNode.name
-      if oldName then
+      if oldNode.name then
         for _, newNode in ipairs(v.data.nodes) do
-          if newNode.name == oldName then
+          if not newNode.name then
+            if wheelNodes[newNode.cid] then
+              newNode.name = wheelNodes[newNode.cid]
+            else
+              print("No name found for: " .. tostring(newNode.cid) .. " " .. tostring(newNode.partPath))
+            end
+          end
+          if newNode.name == oldNode.name then
             if tostring(cid) ~= tostring(newNode.cid) then
-              nodeCount = nodeCount + 1
-              --nodeStatement = nodeStatement .. "Name: " .. oldName .. " Old: " .. tostring(cid) .. " New: " .. tostring(newNode.cid) .. "\n"
               nodeConvert[cid] = newNode.cid
             end
             break
@@ -418,10 +457,6 @@ local function loadNodeIDconvert(separateOldNodes)
         end
       end
     end
-    if nodeStatement ~= "" then
-      print(nodeStatement)
-    end
-    print("Converted " .. nodeCount .. " nodes")
     ::continue::
   end
   print("Checked " .. partCount .. " parts")
@@ -432,14 +467,14 @@ local function loadBeamIDconvert(separateOldBeams, hydros)
   if not separateOldBeams or separateOldBeams == {} then
     return
   end
-  
+
   local separateNewBeams
   if hydros then
     separateNewBeams = seperateByPart(v.data.hydros)
   else
     separateNewBeams = seperateByPart(v.data.beams)
   end
-  
+
   -- Process each part's beams
   for partPath, states in pairs(separateOldBeams) do
     -- Skip if this part doesn't exist in the new vehicle
@@ -455,28 +490,28 @@ local function loadBeamIDconvert(separateOldBeams, hydros)
     if not beams then
       goto continue
     end
-    
+
     -- For each old beam in this part
     for oldCID, oldBeam in pairs(beams) do
       -- Get the nodes this beam connects
       local oldNode1 = oldBeam.idents[1]
       local oldNode2 = oldBeam.idents[2]
       local oldBeamType = oldBeam.idents[3]
-      
+
       -- Convert old node IDs to new node IDs using nodeIDconvert
-      local newNode1 = nodeIDconvert[oldNode1] or oldNode1
-      local newNode2 = nodeIDconvert[oldNode2] or oldNode2
-      
+      local newNode1 = nodeIDconvert[tostring(oldNode1)] or oldNode1
+      local newNode2 = nodeIDconvert[tostring(oldNode2)] or oldNode2
+
       -- Skip if either node conversion failed
       if not newNode1 or not newNode2 then
         goto nextBeam
       end
-      
+
       -- Find matching beam in the new vehicle
       for _, newBeam in ipairs(separateNewBeams[partPath]) do
         -- Check if this beam connects the same nodes (in either order)
         if (newBeam.id1 == newNode1 and newBeam.id2 == newNode2 and newBeam.beamType == oldBeamType) or
-           (newBeam.id1 == newNode2 and newBeam.id2 == newNode1 and newBeam.beamType == oldBeamType) then
+          (newBeam.id1 == newNode2 and newBeam.id2 == newNode1 and newBeam.beamType == oldBeamType) then
           -- Found a match - map old beam CID to new beam CID
           if hydros then
             if oldCID ~= newBeam.cid then
@@ -490,15 +525,63 @@ local function loadBeamIDconvert(separateOldBeams, hydros)
           break
         end
       end
-      
+
       ::nextBeam::
     end
-    
+
     ::continue::
   end
-  
+
   return beamIDconvert
-end 
+end
+
+local function getBeamsToChange(partStates, partsToChange)
+  if not partsToChange then
+    return {}
+  end
+  local changedNodes = {}
+  for partPath, state in pairs(partStates) do
+    if partsToChange and shouldProcessPart(partPath, partsToChange) then
+      if state.nodes then
+        for cid, node in pairs(state.nodes) do
+          if node.name then
+            table.insert(changedNodes, cid, partPath)
+          end
+        end
+      end
+    end
+  end
+  local beamsToChange = {}
+  for partPath, state in pairs(partStates) do
+    if state.beams then
+      for cid, beam in pairs(state.beams) do
+        if beam.idents[1] and beam.idents[2] then
+          if changedNodes[beam.idents[1]] and changedNodes[beam.idents[2]] then
+            goto nextBeam
+          elseif changedNodes[beam.idents[1]] then
+            local node = partStates[changedNodes[beam.idents[1]]].nodes[tostring(beam.idents[1])]
+            if node then
+              table.insert(beamsToChange, cid, {
+                node = beam.idents[1],
+                pos = vec3(node.pos)
+              })
+            end
+          elseif changedNodes[beam.idents[2]] then
+            local node = partStates[changedNodes[beam.idents[2]]].nodes[tostring(beam.idents[2])]
+            if node then
+              table.insert(beamsToChange, cid, {
+                node = beam.idents[2],
+                pos = vec3(node.pos)
+              })
+            end
+          end
+        end
+        ::nextBeam::
+      end
+    end
+  end
+  return beamsToChange
+end
 
 local function loadDamageData(filename, partsToRepair, partsToRemove)
   if filename == nil then
@@ -524,10 +607,14 @@ local function loadDamageData(filename, partsToRepair, partsToRemove)
   -- Calculate rotation difference between saved and current orientation
   local currentRotation = quat(obj:getRotation())
   local saveRotation = quat(save.rotation)
-  
+
   -- Calculate the rotation needed to go from saved rotation to current rotation
   local rotationDifference = currentRotation * saveRotation:inversed()
 
+  -- Adjust beam lengths
+  local beamsToAdjust = getBeamsToChange(save.partStates, partsToRepair)
+  local beamsToRemove = getBeamsToChange(save.partStates, partsToRemove)
+  
   for partPath, state in pairs(save.partStates) do
     if partsToRemove and shouldProcessPart(partPath, partsToRemove) then
       print("Skipping part: " .. partPath .. " because it is in the partsToRemove list")
@@ -544,21 +631,20 @@ local function loadDamageData(filename, partsToRepair, partsToRemove)
   beamIDconvert = loadBeamIDconvert(save.partStates)
   hydrosIDconvert = loadBeamIDconvert(save.partStates, true)
 
+  dump(beamIDconvert)
+
   for partPath, state in pairs(save.partStates) do
     if save.partStates[partPath].nodes then
       for cid, node in pairs(state.nodes) do
-        if nodeIDconvert[cid] then
-          --print("using convert: " .. tostring(nodeIDconvert[cid]) .. " for " .. tostring(node.name))
-        end
-        local newCID = nodeIDconvert[cid] or cid
+        local newCID = nodeIDconvert[tostring(cid)] or cid
         if newCID then
           -- Apply rotation difference to the node position
           local nodePos = vec3(node.pos)
           local rotatedPos = rotationDifference * nodePos
-          
+
           -- Set the rotated position
           obj:setNodePosition(newCID, rotatedPos)
-          
+
           if node.mass then
             obj:setNodeMass(newCID, node.mass)
           end
@@ -567,6 +653,10 @@ local function loadDamageData(filename, partsToRepair, partsToRemove)
     end
     if save.partStates[partPath].beams then
       for cid, beam in pairs(state.beams) do
+        if beamsToAdjust[tonumber(cid)] or beamsToRemove[tonumber(cid)] then
+          print("Skipping beam: " .. tostring(cid) .. " because it is in the beamsToAdjust or beamsToRemove list")
+          goto nextBeam
+        end
         if beam.length > 0 then
           obj:setBeamLength(beamIDconvert[cid] or cid, beam.length)
         end
@@ -577,6 +667,7 @@ local function loadDamageData(filename, partsToRepair, partsToRemove)
           onBeamDeformed(beamIDconvert[cid] or cid, beam.deformed)
           beamstate.onBeamDeformed(beamIDconvert[cid] or cid, beam.deformed)
         end
+        ::nextBeam::
       end
     end
     if save.partStates[partPath].hydros then
